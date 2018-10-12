5146c130786b41cef747e9ce94e092764c46576a
Achira Trakansutleart <achirat@bluesharp.co.th>
2018-10-12 18:52:47 +0700

    change bg

--- app/src/main/java/th/co/bluesharp/smack/CreateUserActivity.kt
+++ app/src/main/java/th/co/bluesharp/smack/CreateUserActivity.kt
@@ -1,18 +1,29 @@
 package th.co.bluesharp.smack
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
 
+
+    var userAvartar = "profiledefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun changeBgBtnClicked(view: View) {
-
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+        avatarColor = "[${r.toDouble() / 255}, ${g.toDouble() / 255}, ${b.toDouble() / 255}, 1]"
+        createUserImage.setBackgroundColor(Color.rgb(r, g, b))
     }
 
     fun createUserBtnClicked(view: View) {
@@ -20,6 +31,16 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun changeImageClicked(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+        userAvartar = when (color) {
+            0 -> "light${avatar}"
+            else -> "dark${avatar}"
+        }
+
+        val resorceId = resources.getIdentifier(userAvartar, "drawable", packageName)
+        createUserImage.setImageResource(resorceId)
 
     }
 }
