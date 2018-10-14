7a1f86cfe0905eee972ee09a3880d3665ae04a4a
Achira Trakansutleart <achirat@bluesharp.co.th>
2018-10-14 16:30:00 +0700

    add dialog

--- app/src/main/java/th/co/bluesharp/smack/Controller/MainActivity.kt
+++ app/src/main/java/th/co/bluesharp/smack/Controller/MainActivity.kt
@@ -9,8 +9,11 @@ import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
@@ -25,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        hideKeyboard()
 
 
         val toggle = ActionBarDrawerToggle(
@@ -78,11 +81,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addBtnNavClicked(view:View){
+        if (AuthService.isLoggedIn) {
+            val builder = AlertDialog.Builder(this)
+            val dialog = layoutInflater.inflate(R.layout.add_channel_diag, null)
+            builder.setView(dialog)
+                    .setPositiveButton("Add") { dialogInterface, i ->
+                        val nameTextField = dialog.findViewById<EditText>(R.id.addChannelName)
+                        val descTextField = dialog.findViewById<EditText>(R.id.addChannelDescription)
+                        hideKeyboard()
 
+                    }.setNegativeButton("Cancel") { dialogInterface, i ->
+                        hideKeyboard()
+                    }.show()
+        }
     }
 
     fun sendMsgBtnClicked(view:View){
 
     }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
+
+    }
+
+
 
 }
