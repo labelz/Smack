7e8207589bc0087b6a0767fb79d027ae5760b0e6
Achira Trakansutleart <achirat@bluesharp.co.th>
2018-10-14 16:09:16 +0700

    lesson-16

--- app/src/main/java/th/co/bluesharp/smack/Controller/LoginActivity.kt
+++ app/src/main/java/th/co/bluesharp/smack/Controller/LoginActivity.kt
@@ -1,10 +1,13 @@
 package th.co.bluesharp.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import th.co.bluesharp.smack.R
 import th.co.bluesharp.smack.Services.AuthService
@@ -15,6 +18,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginCreateBtnClicked(view: View) {
@@ -24,7 +28,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginBtnClicked(view: View) {
-
+        hideKeyboard()
+        enableSpinner(true)
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
         AuthService.loginUser(this, email, password) { complete ->
@@ -34,10 +39,38 @@ class LoginActivity : AppCompatActivity() {
                         val userDataChange = Intent(BOARDCAST_USER_DATA_CHANGE)
                         LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
                         finish()
+                        enableSpinner(false)
+                    } else {
+                        errorToast()
                     }
                 }
+            } else {
+                errorToast()
             }
         }
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginBtn.isEnabled = !enable
+        loginCreateBtn.isEnabled = !enable
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
+
     }
 
 }
